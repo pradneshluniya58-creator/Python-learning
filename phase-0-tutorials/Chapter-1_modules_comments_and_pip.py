2d97c2efda6addb9ade88f4b9643d8f23c797b5f
Pradnesh Santosh Luniya <pradneshluniya58@gmail.com>
2025-11-29 18:22:57 +0530

    added a pip to chapter 1 and also created a virtual environment

--- phase-0-tutorials/Chapter-1_modules_comments_and_pip.py
+++ phase-0-tutorials/Chapter-1_modules_comments_and_pip.py
@@ -3,3 +3,7 @@ print("waiting for two seconds")
 time.sleep(2) #Using a fumction from module time
 print("waiting completed")
 # '#' is comment 
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hi ")
+engine.runAndWait()
