b139adb9240f19a70d6b8539d00b92fe235e3de1
Pradnesh Santosh Luniya <pradneshluniya58@gmail.com>
2025-11-30 05:19:12 +0530

    Added another method to change the input to int from string

--- phase-0-tutorials/Chapter-2_variables_and_datatypes.py
+++ phase-0-tutorials/Chapter-2_variables_and_datatypes.py
@@ -2,3 +2,7 @@
 a = input("Enter the first number : ")
 b = input("Enter the second number : ")
 print("The sum is :",int(a)+int(b))
+#           OR WE CAN DO
+c = int(input("Enter the first number : "))
+d = int(input("Enter the Second number : "))
+print("The sum of the two numbers is :",c+d)
