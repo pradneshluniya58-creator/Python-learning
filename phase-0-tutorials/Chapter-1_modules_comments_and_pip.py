af20627adaf6b599311fe2e97ff1d4788804c6be
Pradnesh Santosh Luniya <pradneshluniya58@gmail.com>
2025-11-29 17:23:43 +0530

    modules,comment and pip introduction

---
+++ phase-0-tutorials/Chapter-1_modules_comments_and_pip.py
@@ -0,0 +1,5 @@
+import time #Importing the module named time , it comes with python so no need to use "pip install time"
+print("waiting for two seconds")
+time.sleep(2) #Using a fumction from module time
+print("waiting completed")
+# '#' is comment 
