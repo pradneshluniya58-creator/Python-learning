1c9832c83752a84bb147a8c50f5a3e52bb2b35e0
Pradnesh Santosh Luniya <pradneshluniya58@gmail.com>
2025-11-30 17:35:02 +0530

    Learned the conditional statements(if and else)

---
+++ phase-0-tutorials/Chapter_6_if_and_else.py
@@ -0,0 +1,5 @@
+# Really Really easy as already know in deep from when I learned c , syntax almost same as c
+# lower can convert string to lower case
+# the python if else is intresting and offers better options to do things 
+# search online , ask AI's and read reasearch paper , if you still can't understand then watch video again
+# remember that the python language is case sensitive while solving problems
