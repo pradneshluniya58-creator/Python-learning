e80f06a7472487fa2001805203c20ff5b951ef2a
Pradnesh Santosh Luniya <pradneshluniya58@gmail.com>
2025-11-30 06:14:21 +0530

    added a chain replace example

--- phase-0-tutorials/Chapter-3_strings.py
+++ phase-0-tutorials/Chapter-3_strings.py
@@ -6,3 +6,11 @@
 # name.capitalize()
 # name.find("")
 # name.replace("old word","new word")
+# chain replace example 
+a = input("Enter the name : ")
+b = input("Enter the date : ")
+c = '''dear name,
+You are selected
+date
+'''
+print(c.replace("name",a).replace('date',b))
