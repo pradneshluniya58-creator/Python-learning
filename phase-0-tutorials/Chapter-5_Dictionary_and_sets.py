24d9f66b2524ca938a4979ee046a43231be371d4
Pradnesh Santosh Luniya <pradneshluniya58@gmail.com>
2025-11-30 07:42:47 +0530

    added a few examples and common functions with syntax for dictionaries

---
+++ phase-0-tutorials/Chapter-5_Dictionary_and_sets.py
@@ -0,0 +1,23 @@
+#Dictionary example and a few common functions
+oxford = {
+    "key" : "something",
+    "gift" : "something willingly given to someone to appreciate",
+    "this" : "A keyword in C++",
+    "mylist"  : [3,5,78,85],
+}
+# print(oxford)
+
+# print(oxford["this"])
+
+# print(oxford.items())
+# for a,b in oxford.items():     #using the for loop
+#     print(a,":",b)             #using the for loop
+
+# print(oxford.keys()) #prints just the keys
+# for a in oxford.keys():         #using the for loop
+#     print(a)                    #using the for loop
+
+# oxford.update({"key" : "nothing","youtube" : "A video sharing platform"}) #updates the dictionary
+# for a,b in oxford.items():     
+#     print(a,":",b)             
+
