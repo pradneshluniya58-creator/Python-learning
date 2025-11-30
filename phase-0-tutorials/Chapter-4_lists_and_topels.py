af19e0fef7a6aca93028d1e9b8ea707118dede33
Pradnesh Santosh Luniya <pradneshluniya58@gmail.com>
2025-11-30 06:46:21 +0530

    Wrote a few common list functions in comments

---
+++ phase-0-tutorials/Chapter-4_lists_and_topels.py
@@ -0,0 +1,7 @@
+# Some common list functions
+#listname.sort() //Sorts the given list
+#listname.reverse() //reverses the given list
+#listname.append(a) //adds a at the end of the given list
+#listname.iinser(i,a) //adds a at the i index of the given list
+#listname.pop(i) // removes the element at index i , removes the end element if unspecified
+#listname.remove(a) // Removes The fist occurence given value(int,str,list etc) from the list
