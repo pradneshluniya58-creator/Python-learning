f83fa5682b9dbc52fdd222b3603affc28288b070
Pradnesh Santosh Luniya <pradneshluniya58@gmail.com>
2025-11-30 07:01:24 +0530

    added a few common tuple functions

--- phase-0-tutorials/Chapter-4_lists_and_tuples.py
+++ phase-0-tutorials/Chapter-4_lists_and_tuples.py
@@ -2,9 +2,11 @@
 #listname.sort() //Sorts the given list
 #listname.reverse() //reverses the given list
 #listname.append(a) //adds a at the end of the given list
-#listname.iinser(i,a) //adds a at the i index of the given list
+#listname.insert(i,a) //adds a at the i index of the given list
 #listname.pop(i) // removes the element at index i , removes the end element if unspecified
 #listname.remove(a) // Removes The fist occurence given value(int,str,list etc) from the list
 #list is written like L1 = [a , b,c ,   d ,e ,f ,g ,h , "pradv"] etc
 #tuple is imutable , can't change ie it is const throught the program
 # tuple ia written like a = () or (1, e ,r ,"oaw b") etc  
+# tuplename.count(a) //Gives the number of times a occured in the tuple
+# tuplename.index(a) //Gives the index of the first occurence of the given value in the tuple
